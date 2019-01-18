751e10e3903ad5b66e60f5b9449e318ee6277255
xumincheng <xumincheng@clubfactory.com>
2019-01-18 17:07:58 +0800

    删除特殊配置

--- src/main/java/com/nickc/todo/config/FilterConfiguration.java
+++ src/main/java/com/nickc/todo/config/FilterConfiguration.java
@@ -6,11 +6,7 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/****************************************
- * creatTime:  下午4:01 2018/10/12
- *    author:  weipenghuang
- *        QQ： 280795290
- ****************************************/
+
 @Configuration
 public class FilterConfiguration {
 
