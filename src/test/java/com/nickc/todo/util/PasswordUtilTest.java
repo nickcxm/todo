09543d7ea20bd304d1a9d02e99d804a5c6d39461
xumincheng <xumincheng@clubfactory.com>
2019-01-29 13:50:42 +0800

    添加测试类

---
+++ src/test/java/com/nickc/todo/util/PasswordUtilTest.java
@@ -0,0 +1,27 @@
+package com.nickc.todo.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by 程旭敏 on 2019/1/29.
+ */
+public class PasswordUtilTest {
+
+    @Test
+    public void genPass() {
+    }
+
+    @Test
+    public void verify() {
+    }
+
+    @Test
+    public void genSalt() {
+    }
+
+    @Test
+    public void md5Hex() {
+    }
+}
